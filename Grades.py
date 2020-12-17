403ff7ab9260541e72e4c82c37b7b6f941f83c90
Wengi <74740339+wengio3@users.noreply.github.com>
2020-12-17 15:08:23 -0500

    Create Grades.py

---
+++ Grades.py
@@ -0,0 +1,37 @@
+# What is the score?
+
+score = int(input("What is your test score?"))
+
+# Determine the Grade.
+if score >= 90:
+    print('Your grade is A.')
+else:
+    if score >= 80:
+        print('Your grade is a B.')
+    else:
+        if score >= 70:
+            print('Your grade is a C.')
+        else:
+            if score >= 60:
+                print('Your grade is a D.')
+            else:
+                print('Your grade is an F.')
+              
+    
+# What is the score?
+
+score = int(input("What is your test score?")) 
+
+# Determine the grade.
+
+if score >= 90: print('your grade is an A')
+
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')   
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.') 
+        
