11a78d85adc6b265d151c8cf5de5b93cbe84c689
Wengi <74740339+wengio3@users.noreply.github.com>
2020-12-18 21:57:24 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -4,7 +4,6 @@
  # 3. The percentage of votes each candidate won
  # 4. The total number of votes each candidate won
  # 5. The winner of the election based on popular vote.
-
 import csv
 import os
 # Assign a variable for the file to load and the path.
@@ -14,3 +13,20 @@ with open(file_to_load) as election_data:
 
     # Print the file object.
      print(election_data)
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the open() function with the "w" mode we will write data to the file.
+open(file_to_save, "w")
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+  #Write three countries to the file  
+    txt_file.write("Counties in the election\n------------------------\nArapahoe\nDenver\nJefferson")
+
+# Close the file
+txt_file.close()
+
+
