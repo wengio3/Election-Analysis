929ca35f843bed6fbaeb8c7fb646e44b35081b0c
Wengi <74740339+wengio3@users.noreply.github.com>
2020-12-18 22:24:50 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -14,9 +14,10 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-      # To do: read and analyze the data here.
-          # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
-        # Print each row in the CSV file.
-    for row in file_reader:
-        print(row)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+    
