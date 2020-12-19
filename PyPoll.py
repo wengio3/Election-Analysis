8d6593675b1dbf5f50050b78cfc2f8ab04e19500
Wengi <74740339+wengio3@users.noreply.github.com>
2020-12-18 20:12:13 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -5,4 +5,12 @@
  # 4. The total number of votes each candidate won
  # 5. The winner of the election based on popular vote.
 
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
 
+    # Print the file object.
+     print(election_data)
