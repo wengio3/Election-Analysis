85f59d42d4e8f8ac8c0261b002942ff449d5c89d
Wengi <74740339+wengio3@users.noreply.github.com>
2020-12-18 22:22:34 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -4,29 +4,19 @@
  # 3. The percentage of votes each candidate won
  # 4. The total number of votes each candidate won
  # 5. The winner of the election based on popular vote.
+# Add our dependencies.
 import csv
 import os
-# Assign a variable for the file to load and the path.
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-    # Print the file object.
-     print(election_data)
-
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Using the open() function with the "w" mode we will write data to the file.
-open(file_to_save, "w")
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-  #Write three countries to the file  
-    txt_file.write("Counties in the election\n------------------------\nArapahoe\nDenver\nJefferson")
-
-# Close the file
-txt_file.close()
-
-
+      # To do: read and analyze the data here.
+          # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+        # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
