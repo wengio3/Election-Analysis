67bd73c831c7255250be9265b57d838bbf778ac5
Wengi <74740339+wengio3@users.noreply.github.com>
2020-12-18 19:22:43 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -4,5 +4,19 @@
  # 3. The percentage of votes each candidate won
  # 4. The total number of votes each candidate won
  # 5. The winner of the election based on popular vote.
-f
+#Assign a variable for the filed to load the path.
+
+file_to_load = 'Resources/election_results.cvs'
+
+#Open the election result and read the file.
+
+election_data = open(file_to_load, 'r')
+
+#To do: perform analysis.
+#Close the file.
+
+election_data.close()
+
+#Open the election result and read the file.
+
 
